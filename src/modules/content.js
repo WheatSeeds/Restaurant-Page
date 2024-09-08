a1b1b58b6da0f13671e8035b81065c71ade3444b
WheatSeeds <georg23kuz@gmail.com>
2024-09-08 17:55:14 +0600

    Version 0.0.6.1 / Test of importing images into JS

--- src/modules/content.js
+++ src/modules/content.js
@@ -1,6 +1,7 @@
 import data from '../menuData.json'
 import createHtmlElement from './render';
 import '../styles/content.css'
+import image from '../Img/product photo/Vodka.jpg'
 
 const content = document.createElement('main');
 const catalog = createHtmlElement('div', 'catalog_grid', null)
@@ -9,19 +10,29 @@ content.appendChild(catalog);
 
 for (const [nameCategory, arrayProducts] of Object.entries(data)) {
     const category = createHtmlElement('div', nameCategory, 'category');
-
     const categoryTitle = createHtmlElement('span', null, 'category_title');
     categoryTitle.innerHTML = nameCategory;
-    
     const categoryProduct = createHtmlElement('ul', null, 'category_products');
 
     catalog.appendChild(category)
     category.appendChild(categoryTitle);
     category.appendChild(categoryProduct);
 
-
     for(const product of arrayProducts){
         const card = createHtmlElement('li', product.title, 'product');
+        const cardImg = createHtmlElement('img', null, 'productImg');
+        const cardTitle = createHtmlElement('span', null, 'productTitle');
+        const cardPrice = createHtmlElement('span', null, 'productPrice');
+
+        cardImg.src = image;
+        cardTitle.innerHTML = product.title;
+        cardPrice.innerHTML = product.price;
+
+
+        card.appendChild(cardImg);
+        card.appendChild(cardTitle);
+        card.appendChild(cardPrice);
+
         categoryProduct.appendChild(card);
     }
 }
