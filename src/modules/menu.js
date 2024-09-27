83a7883923b450cda48cd3e343e14f286a205474
WheatSeeds <georg23kuz@gmail.com>
2024-09-27 16:52:06 +0600

    Version 0.1.3.1 / Refactoring menu.js

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -4,48 +4,53 @@ import '../styles/menu.css';
 
 function importAllImages(r) {
     let images = {};
-    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
     return images;
+};
+
+function createProductCategory(nameCategory, arrayProducts){
+    const category = createHtmlElement('div', nameCategory, 'category');
+    const categoryTitle = createHtmlElement('span', null, 'category_title');
+    categoryTitle.innerHTML = nameCategory;
+    const categoryProducts = createHtmlElement('ul', null, 'category_products');
+    category.appendChild(categoryTitle);
+    category.appendChild(categoryProducts);
+    for(const product of arrayProducts){
+        categoryProducts.appendChild(createProductCard(product));
+    }
+    return category;
 }
 
-function menuPage(){
+function createProductCard(product){
     const images = importAllImages(require.context('../Img/product photo/', false, /\.(png|jpe?g|svg)$/));
+
+    const productCard = createHtmlElement('li', product.title, 'product');
+    const productImgWrap = createHtmlElement('div', null, 'product_image_wrap');
+    const productImg = createHtmlElement('img', null, 'productImage')
+    const productTitle = createHtmlElement('span', null, 'productTitle');
+    const productPrice = createHtmlElement('span', null, 'productPrice');
+
+    productImg.src = images[product.photo];
+    productTitle.innerHTML = product.title;
+    productPrice.innerHTML = `Price: ${product.price}`;     
+
+    productImgWrap.appendChild(productImg);
+    productCard.appendChild(productImgWrap);
+    productCard.appendChild(productTitle);
+    productCard.appendChild(productPrice);
+
+    return productCard;
+}
+function menuPage(){
     const menuContent = document.createElement('main');
-    const catalog = createHtmlElement('div', 'catalog_grid', null)
+    const catalog = createHtmlElement('div', 'catalog_grid', null);
+
     menuContent.appendChild(catalog);
-    
+
     for (const [nameCategory, arrayProducts] of Object.entries(data)) {
-        const category = createHtmlElement('div', nameCategory, 'category');
-        const categoryTitle = createHtmlElement('span', null, 'category_title');
-        categoryTitle.innerHTML = nameCategory;
-
-        const categoryProducts = createHtmlElement('ul', null, 'category_products');
-        const line = createHtmlElement('div', null, 'line');
-        
-        catalog.appendChild(category)
-        category.appendChild(categoryTitle);
-        category.appendChild(categoryProducts);
-    
-        for(const product of arrayProducts){
-            const productCard = createHtmlElement('li', product.title, 'product');
-            const productImgWrap = createHtmlElement('div', null, 'product_image_wrap');
-            const productImg = createHtmlElement('img', null, 'productImage')
-            const productTitle = createHtmlElement('span', null, 'productTitle');
-            const productPrice = createHtmlElement('span', null, 'productPrice');
-
-
-            productImg.src = images[product.photo];
-            productTitle.innerHTML = product.title;
-            productPrice.innerHTML = `Price: ${product.price}`;
-            
-            productImgWrap.appendChild(productImg);
-
-            productCard.appendChild(productImgWrap);
-            productCard.appendChild(productTitle);
-            productCard.appendChild(productPrice);
-            categoryProducts.appendChild(productCard);
-        }
+        catalog.appendChild(createProductCategory(nameCategory, arrayProducts));
     }
+
     document.body.appendChild(menuContent);
 }
 
